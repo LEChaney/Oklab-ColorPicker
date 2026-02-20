fe3a7cb3738f31380d836772f8ba7fcb8e9ed09c
Lance Chaney <lance.e.chaney@gmail.com>
2026-02-20 20:42:52 +1300

    Light radiometric intensity

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
 const { app, BrowserWindow, globalShortcut, ipcMain } = require('electron');
+const fs = require('fs');
+const path = require('path');
+
+const boundsFile = path.join(app.getPath('userData'), 'window-bounds.json');
+
+function loadBounds() {
+  try { return JSON.parse(fs.readFileSync(boundsFile, 'utf8')); } catch (_) { return {}; }
+}
+
+function saveBounds() {
+  if (!win) return;
+  try { fs.writeFileSync(boundsFile, JSON.stringify(win.getBounds())); } catch (_) {}
+}
 
 let win;
 app.whenReady().then(() => {
+  const saved = loadBounds();
   win = new BrowserWindow({
-    width: 400, height: 890, backgroundColor: '#1e1e2e',
+    width: saved.width || 400, height: saved.height || 890,
+    x: saved.x, y: saved.y,
+    backgroundColor: '#1e1e2e',
     webPreferences: { nodeIntegration: true, contextIsolation: false }
   });
   win.setMenuBarVisibility(false);
   win.setAlwaysOnTop(true);
   win.loadFile('index.html');
+  win.on('resized', saveBounds);
+  win.on('moved', saveBounds);
   globalShortcut.register('Alt+C', () => win.webContents.executeJavaScript('pickColor()', true));
   ipcMain.on('set-aot', (_, v) => win.setAlwaysOnTop(v));
 });
