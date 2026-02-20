4c2e3481daa55332228952ab38b6e9afaab734ee
Lance Chaney <lance.e.chaney@gmail.com>
2026-02-20 19:35:28 +1300

    Light Lab and Color History

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ const { app, BrowserWindow, globalShortcut, ipcMain } = require('electron');
 let win;
 app.whenReady().then(() => {
   win = new BrowserWindow({
-    width: 400, height: 820, backgroundColor: '#1e1e2e',
+    width: 400, height: 860, backgroundColor: '#1e1e2e',
     webPreferences: { nodeIntegration: true, contextIsolation: false }
   });
   win.setMenuBarVisibility(false);
+  win.setAlwaysOnTop(true);
   win.loadFile('index.html');
   globalShortcut.register('Alt+C', () => win.webContents.executeJavaScript('pickColor()', true));
   ipcMain.on('set-aot', (_, v) => win.setAlwaysOnTop(v));
